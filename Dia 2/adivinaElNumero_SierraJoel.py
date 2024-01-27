dd1ee0e57711632805724d563691121a5d8d1c2e
Santiago <j.santiagolseon@gmail.com>
2024-01-27 17:43:30 -0500

    Trabajo 2 finalizado

---
+++ Dia 2/adivinaElNumero_SierraJoel.py
@@ -0,0 +1,33 @@
+import random
+
+print("Bienvenido a: \n !ADIVINA EL NUMERO¡ \nEl juego consta de:\n1.Habra un numero de 1 a 100\n2.Tu como usuario deberas intentar adivinar el numero\n3.El programa ira arrojando pistas segun te acerques al numero\n4.Tienes un maximo de 10 intenos\n  !Buena Suerte¡")
+
+numPrograma = int
+numUsuario = int
+
+
+numPrograma = random.randint(1,100)
+
+for i in range(1,11,1):
+    while numUsuario != numPrograma:
+        for j in range(1,11,1):
+            print("Intento: ", j)
+            numUsuario = int(input("Intenta adivinar el numero "))
+
+            if i == 10:
+                print("No tienes mas intentos 🙁\nVuelve a intentarlo")
+            else:
+                if numUsuario > numPrograma:
+                    print("Pista: \nEl numero que debes adivinar es menor del que ingresaste 🤫")
+                elif numUsuario < numPrograma:
+                    print("Pista: \nEl numero que debes adivinar es mayor del que ingresaste 🤫")
+            if numUsuario == numPrograma:
+                print("¡ADIVINASTE EL NUMERO!\nEres el GANADOR🏆")
+                break
+    
+        
+
+
+
+
+
