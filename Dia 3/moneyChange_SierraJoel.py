99a38ba1d0932d62d8792c0fa7f96086c3a02600
Santiago <j.santiagolseon@gmail.com>
2024-01-30 07:21:29 -0500

    correccion final de que antes si estaba bien :(

--- Dia 3/moneyChange_SierraJoel.py
+++ Dia 3/moneyChange_SierraJoel.py
@@ -7,6 +7,8 @@ quantityCoin1 = 0
 quantityCoin2 = 0
 quantityCoin3 = 0
 
+counter=[]
+
 def main(quantityCoin):
     return quantityCoin + 1
     
@@ -21,23 +23,17 @@ else:
         for i in range(0,1,1):
             value = value - coin1
             quantityCoin = main(quantityCoin)
-            quantityCoin1 = quantityCoin1 + 1
+            
     while value > 4:
         for i in range(0,1,1):
             value = value - coin2
             quantityCoin = main(quantityCoin)
-            quantityCoin2 = quantityCoin2 + 1
 
     while value >= 1:
         for i in range(0,1,1,):
             value = value - coin3
             quantityCoin = main(quantityCoin)
-            quantityCoin3 = quantityCoin3+1
 
     print(quantityCoin)
-    totalQuantityCoin1 = 1 * quantityCoin1
-    totalQuantityCoin2 = 1 * quantityCoin2
-    totalQuantityCoin3 = 1 * quantityCoin3
-    print("10 = ",  totalQuantityCoin1, " 5 = ", totalQuantityCoin2, " 1 = ", totalQuantityCoin3)
 
 ## Desarrollado por Joel Santiago Sierra Leon - 1097492869
