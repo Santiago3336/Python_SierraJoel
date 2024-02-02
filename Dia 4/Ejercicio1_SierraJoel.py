b67f9a6896fd233f6e9092703c73ab64308698b1
Santiago <j.santiagolseon@gmail.com>
2024-02-01 22:58:19 -0500

    correcciones aplicadas

--- Dia 4/Ejercicio1_SierraJoel.py
+++ Dia 4/Ejercicio1_SierraJoel.py
@@ -1,26 +1,24 @@
-def pares(T_n,n,k):
-    pairs=set()
+def pares(T_n, n, k):
+    pairs = set()
     for i in range(n):
-        for j in range(i+1,n):
-            if (T_n[i]+T_n[j] % k == 0):
-                pairs.add((min(T_n[i],T_n[j]), max(T_n[i],T_n[j])))
-            return len(pairs)
-        
+        for j in range(i + 1, n):
+            if (T_n[i] + T_n[j]) % k == 0:
+                pairs.add((min(T_n[i], T_n[j]), max(T_n[i], T_n[j])))
+    return len(pairs)
+
 T = int(input())
 for case in range(T):
-    texto = input("")
-    numeros = input("")
+    text = input("")
+    nums = input("")
     n = 0
     k = 0
     T_n = list()
-    textoLista = texto.split(' ')
-    numerosLista = numeros.split(' ')
-    #print(textoLista)
+    textoLista = text.split(" ")
+    numsLista = nums.split(" ")
     n = int(textoLista[0])
     k = int(textoLista[1])
-    #print(numerosLista)
     for p in range(n):
-        numerito = int(numerosLista[p])
-        T_n.append(abs(numerito))
-    result = pares (T_n,n,k)
-    print("Case {}:{}".format(case+1,result))
+        number = int(numsLista[p])
+        T_n.append(abs(number))
+    result = pares(T_n, n, k)
+    print("Case {}: {}".format(case + 1, result))
