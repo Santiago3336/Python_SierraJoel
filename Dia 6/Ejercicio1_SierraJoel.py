ea219ac008f2aa682d0f29fb784103d83ea01ae7
Santiago <j.santiagolseon@gmail.com>
2024-02-01 23:08:20 -0500

    falta completar

--- Dia 6/Ejercicio1_SierraJoel.py
+++ Dia 6/Ejercicio1_SierraJoel.py
@@ -3,4 +3,24 @@
 #Y la cantidad, al finalizar mostrar en la consola el precio total, si
 #El producto no esta debe mostrar un mensaje informando sobre ello
 
+diccionario = {'Productos':("Arroz", "Papa", "Yuca", "Aguacate", "Tomate", "Cebolla", "Lechuga", "Ajo", "Pimenton"), 
+               'Precios':(4600, 3600, 4200, 5500, 3400, 3200, 3500, 1200, 3700)}
+print("Bienvenido\n A continuacion se te mostrara el inventario de productos\nJunto con su valor en libras")
+print(diccionario)
+
+producto, cantidad = map(input("Favor ingrese el producto que desea junto con la cantidad")).split()
+
+producto = str
+cantidad = int
+cond = True
+
+
+
+while cond == True:
+    for i in range(0,1,1):
+
+
+        
+
+
 
