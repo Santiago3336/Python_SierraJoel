393bf1e0008810a95f03abac202ca57ce837217d
Santiago <j.santiagolseon@gmail.com>
2024-02-04 11:36:36 -0500

    creacion de la funcion a importar

---
+++ Dia 7/Dia 2/modulo.py
@@ -0,0 +1,2 @@
+def funcionDia2():
+    print("Bienvenido a: \n !ADIVINA EL NUMERO¡ \nEl juego consta de:\n1.Habra un numero de 1 a 100\n2.Tu como usuario deberas intentar adivinar el numero\n3.El programa ira arrojando pistas segun te acerques al numero\n4.Tienes un maximo de 10 intenos\n  !Buena Suerte¡")
