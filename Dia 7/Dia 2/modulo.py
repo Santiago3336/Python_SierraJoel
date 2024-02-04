9d9afb81d932ad7fc2fb5cdba3f03171348fb5c3
Santiago <j.santiagolseon@gmail.com>
2024-02-04 11:42:54 -0500

    funcion de fibo creada

--- Dia 7/Dia 2/modulo.py
+++ Dia 7/Dia 2/modulo.py
@@ -1,2 +1,5 @@
 def funcionDia2():
     print("Bienvenido a: \n !ADIVINA EL NUMERO¡ \nEl juego consta de:\n1.Habra un numero de 1 a 100\n2.Tu como usuario deberas intentar adivinar el numero\n3.El programa ira arrojando pistas segun te acerques al numero\n4.Tienes un maximo de 10 intenos\n  !Buena Suerte¡")
+
+def funcionFiboDia2():
+    return"Bienvenido usuario, a continuacion veras la secuencia Fibonacci"
